57ab1d93d456fa81db5bcabc58097f8a6cb00619
kresovic-milos <kresovic.milos@gmail.com>
2016-10-02 19:27:29 +0200

    initial commit

---
+++ Municipio/src/main/java/com/attozoic/MunicipioApplication.java
@@ -0,0 +1,12 @@
+package com.attozoic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunicipioApplication.class, args);
+	}
+}
