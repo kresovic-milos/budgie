90076ab81e69b911bb8d83e149b07582c5f5b9a1
srda333 <ivansrdic@gmail.com>
2016-10-25 01:19:00 +0200

    Pokusali rebalans

--- Municipio/src/main/java/com/attozoic/main/controllers/ControllerProgramme.java
+++ Municipio/src/main/java/com/attozoic/main/controllers/ControllerProgramme.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.attozoic.main.model.ActiveState;
+import com.attozoic.main.model.Programme;
 import com.attozoic.main.model.ProgrammeGoal;
 import com.attozoic.main.model.SuperEntity;
 import com.attozoic.main.services.ServiceProgramme;
@@ -33,6 +34,26 @@ public class ControllerProgramme {
 		return serviceProgramme.findOne(uid);
 	}
 
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Programme update(@RequestBody Programme programme) {
+		return (Programme) serviceProgramme.save(programme);
+	}
+	
+	@RequestMapping(value="{uid}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void delete(@PathVariable(value="uid") Long uid) {
+		serviceProgramme.delete(uid);
+	}
+	
+	@RequestMapping(value="{uid}/archive", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void archive(@PathVariable(value="uid") Long uid) {
+		serviceProgramme.archive(uid);
+	}
+	
+	@RequestMapping(value="{uid}/unarchive", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void unarchive(@PathVariable(value="uid") Long uid) {
+		serviceProgramme.unarchive(uid);
+	}
+	
 	// Cuva programski cilj za izabrani program
 	@RequestMapping(value="/{uid}/programmeGoal", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ProgrammeGoal addProgrammeGoal(@PathVariable(value="uid") Long uid, @RequestBody ProgrammeGoal programmeGoal) {
