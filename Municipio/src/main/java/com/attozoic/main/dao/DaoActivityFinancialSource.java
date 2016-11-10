7940d12fb5449274fb017fe924e7446c34d44ba0
srda333 <ivansrdic@gmail.com>
2016-11-10 03:59:34 +0100

    Ispravljeno sabiranje vrednosti rebalansa u izvorima finansiranja.

--- Municipio/src/main/java/com/attozoic/main/dao/DaoActivityFinancialSource.java
+++ Municipio/src/main/java/com/attozoic/main/dao/DaoActivityFinancialSource.java
@@ -1,18 +1,26 @@
 package com.attozoic.main.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.attozoic.main.model.ActivityFinancialSource;
+import com.attozoic.main.model.RebalanceOneField;
+import com.attozoic.main.model.RebalancesCount;
 import com.attozoic.main.model.SuperEntity;
 import com.attozoic.main.repositories.RepositoryActivityFinancialSource;
 import com.attozoic.main.repositories.RepositoryEntity;
+import com.attozoic.main.repositories.RepositoryRebalancesCount;
 
 @Repository
 public class DaoActivityFinancialSource extends DaoEntity {
 
 	@Autowired
 	private RepositoryActivityFinancialSource repo;
+	
+	@Autowired
+	private RepositoryRebalancesCount repoRebalanceCount;
 	
 	@SuppressWarnings("rawtypes")
 	@Override
@@ -24,6 +32,16 @@ public class DaoActivityFinancialSource extends DaoEntity {
 	public SuperEntity update(SuperEntity superEntity) {
 		ActivityFinancialSource activityFinancialSource = (ActivityFinancialSource) superEntity;
 		activityFinancialSource.setSumSources123(activityFinancialSource.getSourceBaseYearPlus1() + activityFinancialSource.getSourceBaseYearPlus2() + activityFinancialSource.getSourceBaseYearPlus3());
+		try {
+			RebalancesCount rc = repoRebalanceCount.findOne(new Long(1));
+			int numReb = rc.getRebalancesCount();
+			if (numReb > 0) {
+				List<RebalanceOneField> l = activityFinancialSource.getRebalances();
+				for (int i = 0; i < l.size(); i++) {
+					activityFinancialSource.setSumSources123(activityFinancialSource.getSumSources123() + l.get(i).getValue());
+				}
+			}
+		} catch (NullPointerException ex) {}
 		return super.update(activityFinancialSource);
 	}
 	
--- Municipio/src/main/java/com/attozoic/main/dao/DaoProjectFinancialSource.java
+++ Municipio/src/main/java/com/attozoic/main/dao/DaoProjectFinancialSource.java
@@ -1,18 +1,26 @@
 package com.attozoic.main.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.attozoic.main.model.ProjectFinancialSource;
+import com.attozoic.main.model.RebalanceOneField;
+import com.attozoic.main.model.RebalancesCount;
 import com.attozoic.main.model.SuperEntity;
 import com.attozoic.main.repositories.RepositoryEntity;
 import com.attozoic.main.repositories.RepositoryProjectFinancialSource;
+import com.attozoic.main.repositories.RepositoryRebalancesCount;
 
 @Repository
 public class DaoProjectFinancialSource extends DaoEntity {
 
 	@Autowired
 	private RepositoryProjectFinancialSource repo;
+	
+	@Autowired
+	private RepositoryRebalancesCount repoRebalanceCount;
 	
 	@SuppressWarnings("rawtypes")
 	@Override
@@ -24,6 +32,16 @@ public class DaoProjectFinancialSource extends DaoEntity {
 	public SuperEntity update(SuperEntity superEntity) {
 		ProjectFinancialSource projectFinancialSource = (ProjectFinancialSource) superEntity;
 		projectFinancialSource.setSumSources123(projectFinancialSource.getSourceBaseYearPlus1() + projectFinancialSource.getSourceBaseYearPlus2() + projectFinancialSource.getSourceBaseYearPlus3());
+		try {
+			RebalancesCount rc = repoRebalanceCount.findOne(new Long(1));
+			int numReb = rc.getRebalancesCount();
+			if (numReb > 0) {
+				List<RebalanceOneField> l = projectFinancialSource.getRebalances();
+				for (int i = 0; i < l.size(); i++) {
+					projectFinancialSource.setSumSources123(projectFinancialSource.getSumSources123() + l.get(i).getValue());
+				}
+			}
+		} catch (NullPointerException ex) {}
 		return super.update(projectFinancialSource);
 	}
 	
