446a67b44665bafae903b519ceac0472cd8327d6
srda333 <ivansrdic@gmail.com>
2016-11-18 06:33:20 +0100

    Update BalanceText changed

--- Municipio/src/main/java/com/attozoic/main/dao/DaoActivityGoalIndicator.java
+++ Municipio/src/main/java/com/attozoic/main/dao/DaoActivityGoalIndicator.java
@@ -3,6 +3,9 @@ package com.attozoic.main.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.attozoic.main.model.ActivityGoalIndicator;
+import com.attozoic.main.model.SuperEntity;
+import com.attozoic.main.model.balance.BalanceText;
 import com.attozoic.main.repositories.RepositoryActivityGoalIndicator;
 import com.attozoic.main.repositories.RepositoryEntity;
 
@@ -17,5 +20,5 @@ public class DaoActivityGoalIndicator extends DaoEntity {
 	public RepositoryEntity getRepoEntity() {
 		return repoActivityGoalIndicator;
 	}
-
+	
 }
--- Municipio/src/main/java/com/attozoic/main/dao/daoBalance/DaoBalanceText.java
+++ Municipio/src/main/java/com/attozoic/main/dao/daoBalance/DaoBalanceText.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.attozoic.main.dao.DaoEntity;
+import com.attozoic.main.model.SuperEntity;
+import com.attozoic.main.model.balance.BalanceText;
 import com.attozoic.main.repositories.RepositoryEntity;
 import com.attozoic.main.repositories.repositoriesBalance.RepositoryBalanceText;
 
@@ -17,6 +19,14 @@ public class DaoBalanceText extends DaoEntity {
 	@Override
 	public RepositoryEntity getRepoEntity() {
 		return repoBalanceText;
+	}
+
+	@Override
+	public SuperEntity update(SuperEntity superEntity) {
+		BalanceText bt = (BalanceText)superEntity;
+		BalanceText balanceText = (BalanceText)getRepoEntity().findOne(superEntity.getUid());
+		balanceText.setValue(bt.getValue());
+		return super.update(balanceText);
 	}
 	
 }
